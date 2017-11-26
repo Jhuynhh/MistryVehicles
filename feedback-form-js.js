92ed87fe325136bbe33d4f3802c6239b79b19c09
dhruvmistry96 <32339664+dhruvmistry96@users.noreply.github.com>
2017-11-25 18:07:01 -0800

    Javascript for the Sliding animation of the Feedback form.
    This has the js code for the left sliding animation of the feedback tab.

---
+++ feedback-form-js.js
@@ -0,0 +1,80 @@
+(function ($) {
+$.fn.vAlign = function() {
+	return this.each(function(i){
+	var h = $(this).height();
+	var oh = $(this).outerHeight();
+	var mt = (h + (oh - h)) / 2;	
+	$(this).css("margin-top", "-" + mt + "px");	
+	$(this).css("top", "50%");
+	});	
+};
+$.fn.toggleClick = function(){
+    var functions = arguments ;
+    return this.click(function(){
+            var iteration = $(this).data('iteration') || 0;
+            functions[iteration].apply(this, arguments);
+            iteration = (iteration + 1) % functions.length ;
+            $(this).data('iteration', iteration);
+    });
+};
+})(jQuery);
+$(window).load(function() {
+	//cache
+	$img_control = $("#mrova-img-control");
+	$mrova_feedback = $('#mrova-feedback');
+	$mrova_contactform = $('#mrova-contactform');
+
+	//setback to block state and vertical align to center
+	$mrova_feedback.vAlign()
+	.css({'display':'block','height':$mrova_feedback.outerHeight()});
+	//Aligning feedback button to center with the parent div 
+
+	$img_control.vAlign()
+	//animate the form
+	.toggleClick(function(){
+		$mrova_feedback.animate({'right':'-2px'},1000);
+	}, function(){
+		$mrova_feedback.animate({'right':'-'+$mrova_feedback.outerWidth()},1000);
+	});
+
+	//Form handling
+	$('#mrova-sendbutton').click( function() {
+				var url = 'send.php';
+				var error = 0;
+				
+				
+				
+				$('.required', $mrova_contactform).each(function(i) {
+					if($(this).val() === '') {
+						error++;
+					}
+				});
+				// each
+				if(error > 0) {
+					alert('Please fill in all the mandatory fields. Mandatory fields are marked with an asterisk *.');
+				} else {
+					$str = $mrova_contactform.serialize();
+
+					//submit the form
+					$.ajax({
+						type : "GET",
+						url : url,
+						data : $str,
+						success : function(data) {
+
+							if(data == 'success') {
+								// show thank you
+								$('#mrova-contact-thankyou').show();
+								$mrova_contactform.hide();
+							} else {
+								alert('Unable to send your message. Please try again.');
+							}
+						}
+					});
+					//$.ajax
+
+				}
+				return false;
+			});
+
+});
