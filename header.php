fe7f79e22b4a864b289c2e4b1228c8a30fea10fe
Udit Desai <desaiuditd@gmail.com>
2017-12-10 15:33:26 -0800

    Fixes header urls (#3)
    * refactors home page, people page
    
    * fixes container top margin
    
    * Fixes feedback database connection.
    
    * fixes header urls

--- header.php
+++ header.php
@@ -11,20 +11,20 @@ $current = $_SERVER['SCRIPT_NAME'];
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
 	<div class="container">
-		<a class="navbar-brand" href="/index.php">Mistry Vehicles</a>
+		<a class="navbar-brand" href="./index.php">Mistry Vehicles</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 		<div class="collapse navbar-collapse" id="navbarResponsive">
 			<ul class="navbar-nav ml-auto">
 				<li class="nav-item <?php echo $current === '/index.php' ? 'active' : ''; ?>">
-					<a class="nav-link" href="/index.php">Home</a>
+					<a class="nav-link" href="./index.php">Home</a>
 				</li>
 				<li class="nav-item <?php echo $current === '/people.php' ? 'active' : ''; ?>">
-					<a class="nav-link" href="/people.php">About Us</a>
+					<a class="nav-link" href="./people.php">About Us</a>
 				</li>
 				<li class="nav-item <?php echo $current === '/inventory.html' ? 'active' : ''; ?>">
-					<a class="nav-link" href="/inventory.php">Products</a>
+					<a class="nav-link" href="./inventory.php">Products</a>
 				</li>
 				<li class="nav-item">
 					<a class="nav-link" href="#">Cart</a>
